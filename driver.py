0c820e75780fd5b17a4a93f9fe0724a108444c3c
Charlie <charlieedoherty@gmail.com>
2024-04-17 15:10:23 -0500

    Initial Commit

---
+++ driver.py
@@ -0,0 +1,16 @@
+"""
+Author: Charlie Doherty
+KUID: 3115329
+Date: 4/17/24
+Lab: 09
+Last modified: 4/17/24
+Purpose: Time several methods from Stacks, Queues, and Lists using various values for n through Python's time library.
+
+Run Driver.py to begin the program
+"""
+
+from time import process_time_ns
+
+if __name__ == "__main__":
+
+    print(process_time_ns())
